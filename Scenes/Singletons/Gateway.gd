ff428ae27ed82a4677f79632bd007d3da8212128
Skengrek <armand.masse@cpe.fr>
2022-04-27 10:47:59 +0200

    First Commit

--- Scenes/Singletons/Gateway.gd
+++ Scenes/Singletons/Gateway.gd
@@ -17,4 +17,13 @@ func startServer():
 	custom_multiplayer.set_root_node(self)
 	custom_multiplayer.set_network_peer(network)
 	
-	
+	network.connect('peer_connected', self, '_Peer_Connected')
+	network.connect('peer_disconnected', self, '_Peer_Disconnected')
+
+
+func _Peer_Connected(gateway_id):
+	print(str(gateway_id) + 'Connected')
+
+
+func _Peer_Disconnected(gateway_id):
+	print(str(gateway_id) + 'Disconnected')
